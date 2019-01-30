1439cc007dc92530b21e8798696264ddf0ee9a69
Aymen jegham <jegham.aymen.89@gmail.com>
2019-01-30 04:55:08 +0100

    notification bugs fixed

--- app/src/main/java/com/example/asus/weathercast/Alarm1.java
+++ app/src/main/java/com/example/asus/weathercast/Alarm1.java
@@ -4,7 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.Toast;
+import java.util.Calendar;
+
 
 public class Alarm1 extends BroadcastReceiver {
 
@@ -12,7 +13,26 @@ public class Alarm1 extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent1) {
         String  alarm = intent1.getStringExtra("alarm");
 
-        Log.v("alarmsssss","alarm is morning up"+alarm);
+        Calendar calendar =Calendar.getInstance();
+        Calendar calendar1 =Calendar.getInstance();
+        calendar1.setTimeInMillis(Long.parseLong(alarm));
+
+        int hournow=calendar.get(Calendar.HOUR);
+        int houralarm=calendar1.get(Calendar.HOUR);
+
+    if (houralarm == hournow) {
+
+
+         Log.v("alarmsssss", "alarm is morning up" + hournow + "  " + houralarm);
     }
+
+
+
+
+
+
+
+    }
+
 
 }
--- app/src/main/java/com/example/asus/weathercast/SettingsActivity.java
+++ app/src/main/java/com/example/asus/weathercast/SettingsActivity.java
@@ -9,8 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
+ import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -421,7 +420,7 @@ public class SettingsActivity extends AppCompatActivity  {
     private void setAlarm1(long timeInMillis) {
         AlarmManager alarmManager1 = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         Intent intent1 = new Intent(this,Alarm1.class);
-        intent1.putExtra("alarm", "alarm1");
+        intent1.putExtra("alarm", String.valueOf(timeInMillis));
         PendingIntent pendingIntent1 = PendingIntent.getBroadcast(this,1,intent1,FLAG_UPDATE_CURRENT);
         alarmManager1.setRepeating(AlarmManager.RTC_WAKEUP,timeInMillis,AlarmManager.INTERVAL_DAY,pendingIntent1);
         Toast.makeText(this, "Alarm 1 is set", Toast.LENGTH_SHORT).show();
@@ -430,7 +429,7 @@ public class SettingsActivity extends AppCompatActivity  {
     private void setAlarm2(long timeInMillis) {
         AlarmManager alarmManager2 = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         Intent intent2 = new Intent(this,Alarm1.class);
-        intent2.putExtra("alarm", "alarm2");
+        intent2.putExtra("alarm", String.valueOf(timeInMillis));
         PendingIntent pendingIntent2 = PendingIntent.getBroadcast(this,2,intent2,FLAG_UPDATE_CURRENT);
         alarmManager2.setRepeating(AlarmManager.RTC_WAKEUP,timeInMillis,AlarmManager.INTERVAL_DAY,pendingIntent2);
         Toast.makeText(this, "Alarm 2 is set", Toast.LENGTH_SHORT).show();
@@ -438,7 +437,7 @@ public class SettingsActivity extends AppCompatActivity  {
     private void setAlarm3(long timeInMillis) {
         AlarmManager alarmManager3 = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         Intent intent3 = new Intent(this,Alarm1.class);
-        intent3.putExtra("alarm", "alarm3");
+        intent3.putExtra("alarm", String.valueOf(timeInMillis));
         PendingIntent pendingIntent3 = PendingIntent.getBroadcast(this,3,intent3,FLAG_UPDATE_CURRENT);
         alarmManager3.setRepeating(AlarmManager.RTC_WAKEUP,timeInMillis,AlarmManager.INTERVAL_DAY,pendingIntent3);
         Toast.makeText(this, "Alarm 3 is set", Toast.LENGTH_SHORT).show();
